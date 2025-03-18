52558086554631680c0e07e68cd0481f052489d3
unknown <juanignaciodelossantos01@gmail.com>
2025-03-18 15:36:39 -0300

    feat(services): created fc to fetch data

---
+++ src/lib/services/characters.ts
@@ -0,0 +1,27 @@
+import { ApiResponse } from "@/types/api";
+import { Character } from "@/types/character";
+import { RICK_AND_MORTY_API_URL } from "@/utils/consts";
+
+export const getAllCharacters = async (
+  page: number
+): Promise<ApiResponse<Character>> => {
+  const res = await fetch(`${RICK_AND_MORTY_API_URL}/character/?page=${page}`);
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  const data: ApiResponse<Character> = await res.json();
+
+  return data;
+};
+
+export const getCharacterById = async (characterIds: string) => {
+  const res = await fetch(
+    `${RICK_AND_MORTY_API_URL}/character/${characterIds}`
+  );
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  const data: Character = await res.json();
+
+  return data;
+};
---
+++ src/lib/services/episodes.ts
@@ -0,0 +1,15 @@
+import { Episode } from "@/types/episode";
+import { RICK_AND_MORTY_API_URL } from "@/utils/consts";
+
+export const getEpisodesById = async (episodesIds?: string[]) => {
+  if (!episodesIds || !(episodesIds.length > 0)) return null;
+  const res = await fetch(`${RICK_AND_MORTY_API_URL}/episode/${episodesIds}`);
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  const data: Episode[] | Episode = await res.json();
+
+  const normalizedData = Array.isArray(data) ? data : [data];
+
+  return normalizedData;
+};
