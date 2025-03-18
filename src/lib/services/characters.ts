ee0e58832a22d95aab89883c9670ca012b7c0bb7
unknown <juanignaciodelossantos01@gmail.com>
2025-03-18 15:54:32 -0300

    refactor(services): removed unnecessary types and consolidated returned values

--- src/lib/services/characters.ts
+++ src/lib/services/characters.ts
@@ -2,9 +2,7 @@ import { ApiResponse } from "@/types/api";
 import { Character } from "@/types/character";
 import { RICK_AND_MORTY_API_URL } from "@/utils/consts";
 
-export const getAllCharacters = async (
-  page: number
-): Promise<ApiResponse<Character>> => {
+export const getAllCharacters = async (page: number) => {
   const res = await fetch(`${RICK_AND_MORTY_API_URL}/character/?page=${page}`);
 
   if (!res.ok) throw new Error("Failed to fetch data");
--- src/lib/services/episodes.ts
+++ src/lib/services/episodes.ts
@@ -2,7 +2,7 @@ import { Episode } from "@/types/episode";
 import { RICK_AND_MORTY_API_URL } from "@/utils/consts";
 
 export const getEpisodesById = async (episodesIds?: string[]) => {
-  if (!episodesIds || !(episodesIds.length > 0)) return null;
+  if (!episodesIds || !(episodesIds.length > 0)) return [];
   const res = await fetch(`${RICK_AND_MORTY_API_URL}/episode/${episodesIds}`);
 
   if (!res.ok) throw new Error("Failed to fetch data");
